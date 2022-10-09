d906f3ac424662e2537bdfd7548d732ae63ff1ba
ADIL SHIEKH <114225192+Darkranger00@users.noreply.github.com>
2022-10-09 23:27:14 +0530

    Update commands.py

--- crushbot/helpers/commands.py
+++ crushbot/helpers/commands.py
@@ -3,23 +3,25 @@ ALIVE_CMD = """
 Check Bot Alive Or What!
 
 ✮ `/alive` - To Check Am I Alive! - `(SUDO:- True)`
+***------------------------------------------***
 """
 
 PING_CMD =  """
 Check Bot Speed Using Ping!
 
 ✮ `/ping` - To Check My Ping! - `(SUDO:- True)`
+***------------------------------------------***
 """
 
 RAID_CMD = """
-To Raid On Enemies!
+To Fuck out The Enemies!
 
 ✮ `/mraid` - To Start Mention Raid On Enemies! Use In Reply Of User Message Or Give Username! - `(SUDO:- True)`
-
+***---------------------------------------------------------------------------------------------------------***
 ✮ `/sraid` - To Stop Mention Raid On Enemies! Use In Reply Of User Message Or Give Username! - `(SUDO:- True)`
-
+***---------------------------------------------------------------------------------------------------------***
 ✮ `/raid` - To Start Raid On Enemy! - `(SUDO:- True)`
-
+***---------------------------------------------------------------------------------------------------------***
 ✮ `/draid` - To  Stop Raid On Enemy! - `(SUDO:- True)`
 """
 
@@ -27,7 +29,7 @@ REPLYRAID_CMD = """
 To Reply Raid On Enemies!
 
 ✮ `/replyraid` - To Start Reply Raid On Enemies! Use In Reply Of User Message Or Give Username! - `(SUDO:- True)`
-
+***---------------------------------------------------------------------------------------------------------***
 ✮ `/dreplyraid` - To Stop Reply Raid On Enemies! Use In Reply Of User Message Or Give Username! - `(SUDO:- True)`
 """
 
@@ -35,13 +37,13 @@ SPAM_CMD = """
 To Spam Messages In Groups!
 
 ✮ `/spam` - Give A Text Or Reply To Message To Spam In Range Of 99 Messages! - `(SUDO:- True)`
-
+***---------------------------------------------------------------------------------------------------------***
 ✮ `/bigspam` - Give A Text Or Reply To Message To Big Spam In Range Of 99999 Messages! - `(SUDO:- True)`
-
+***---------------------------------------------------------------------------------------------------------***
 ✮ `/mspam` - Reply To A Media To Spam In Range Of 99999 Messages! - `(SUDO:- True)`
-
+***---------------------------------------------------------------------------------------------------------***
 ✮ `/uspam` - Give A Text Or Reply To Message To Unlimited Spam Messages! - `(SUDO:- True)`
-
+***---------------------------------------------------------------------------------------------------------***
 ✮ `/ustop` - To Stop Unlimited Spam Messages! - `(SUDO:- True)`
 """
 
@@ -49,7 +51,7 @@ PSPAM_CMD = """
 To Spam Porn In Groups!
 
 ✮ `/pspam` - To Spam Porn  In Groups! Also You Can Put Text In Caption Giving Text Or Replying To Message! - `(SUDO:- True)`
-
+***-----------------------------------------------------------------------------------------------------------------------***
 ✮ `/pstop` - To Stop Porn Spam! - `(SUDO:- True)`
 """
 
@@ -57,6 +59,6 @@ EXTRA_CMD = """
 To Add Extra Modules In Spam Bot! Only Work In Master Bot (Master Bot: Which Running On Bot Token No. 1!)!
 
 ✮ `/install` - To Install Extra Plugin In SpamBot! - `(SUDO:- True)`
-
+***----------------------------------------------------------------***
 ✮ `/uninstall` - To Uninstall Plugin In SpamBot! - `(SUDO:- True)`
 """
