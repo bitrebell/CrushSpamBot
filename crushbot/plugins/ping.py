6d7404e26a659378fc0cc87c568c9143f87328ef
ADIL SHIEKH <114225192+Darkranger00@users.noreply.github.com>
2022-10-10 00:34:32 +0530

    Update ping.py

--- crushbot/plugins/ping.py
+++ crushbot/plugins/ping.py
@@ -3,6 +3,8 @@ from crushbot import CrushBot1, CrushBot2, CrushBot3, CrushBot4, CrushBot5
 from telethon import events
 from datetime import datetime
 
+PING_PIC = "https://te.legra.ph/file/c037be45ae763bde8c892.jpg"
+
 @CrushBot1.on(events.NewMessage(incoming=True, pattern='/ping'))
 @CrushBot2.on(events.NewMessage(incoming=True, pattern='/ping'))
 @CrushBot3.on(events.NewMessage(incoming=True, pattern='/ping'))
@@ -14,4 +16,4 @@ async def ping(e):
         message = await e.client.send_message(e.chat_id, "`Pinging.....!`")
         after = datetime.now()
         ms = (after - before).microseconds / 1000
-        await e.client.edit_message(message, f"Ping Pong!\n\nCrush Spam Bot\n\nMy Master:- [{OWNER_NAME}](tg://user?id={OWNER_ID})\n\nPing:- {ms} ms\n\nCrush Spam Bot On Fire")
+        await e.client.edit_message(message, PING_PIC, f"𝐏𝐈𝐍𝐆 𝐏𝐎𝐍𝐆!!!\n***--------------***\n𝐂𝐑𝐔𝐒𝐇 𝐒𝐏𝐀𝐌 𝐁𝐎𝐓\n***--------------***\n𝒎𝒚 𝒎𝒂𝒔𝒕𝒆𝒓:- [{OWNER_NAME}](tg://user?id={OWNER_ID})\n***--------------***\n𝐏𝐈𝐍𝐆!!:- {ms} ms\n***--------------***\n𝐂𝐑𝐔𝐒𝐇 𝐒𝐏𝐀𝐌 𝐁𝐎𝐓 𝐎𝐍 𝐅𝐈𝐑𝐄༗.")
