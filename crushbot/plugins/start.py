6fccbbfd417b87b5f915c00791039da33aa38e21
ADIL SHIEKH <114225192+Darkranger00@users.noreply.github.com>
2022-10-17 17:47:09 +0530

    Update start.py

--- crushbot/plugins/start.py
+++ crushbot/plugins/start.py
@@ -5,7 +5,7 @@ from telethon import events, Button
 
 data  = [
     Button.url("Channel", url="t.me/crushbot_support"),
-    Button.url("Repo", url="https://GitHub.com/Darkranger00/"),
+    Button.url("Repo", url="https://GitHub.com/Darkranger00/CrushSpamBot"),
     Button.url("Group", url="https://t.me/zgarmy1")
 ]
 
